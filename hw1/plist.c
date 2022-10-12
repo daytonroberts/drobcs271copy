3ceb3b11c1860610799b9581936a409241167eed
daytonroberts <89813195+daytonroberts@users.noreply.github.com>
2022-10-12 14:12:14 -0700

    not final

--- hw1/plist.c
+++ hw1/plist.c
@@ -12,26 +12,55 @@ struct plist_struct
 
 plist pl_new()
 {
-	return NULL;
+	plist ret = malloc(sizeof(struct(plist_struct));
+	ret -> data = 0x4;
+	ret -> next = malloc(ret -> data * sizeof(struct(plist_struct));
+	return ret;
 }
 
 void pl_free(plist l)
 {
+	free(l);
 	return;
 }
 
 void pl_print(plist l)
 {
+	printf("[");
+	for (int i = 0; i < sizeof(l)/sizeof(int); i++)
+	{
+		if i == sizeof(l)/sizeof(int) - 1 {
+			printf(("%d, ", l->data);
+		}
+		else {
+			printf("%d", l->data);
+		}
+	}
+	printf("]");
 	return;
 }
 
 void pl_append(plist l, void *x)
 {
+	if l->next {
+		pl_append(l->next, x)
+	}
+	else { 
+		plist ln = pl_new();
+		ln -> data = x
+		l -> next = ln
+	}
 	return;
 }
 
 void pl_extend(plist l1, plist l2)
 {
+	if l1 -> next {
+		pl_extend(l1->next, l2)
+	}
+	else {
+		l1->next = l2
+	}
 	return;
 }
 
